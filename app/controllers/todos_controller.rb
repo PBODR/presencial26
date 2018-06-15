5d435a1cfebdaa1481a9a1430c385073657af5ab
pbodr <pbolivares@gmail.com>
2018-06-15 19:46:48 -0400

    segundo commit: show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,29 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todos_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  private
+  def todos_params #scope
+    params.require(:todo).permit(:description, :completed)
+
+  end
 end
